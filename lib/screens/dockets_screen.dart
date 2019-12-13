2cceb631b04da74a8b9b1c811dbf5e29b704f6c7
Prashant <prashant.patel77@gmail.com>
2019-12-13 14:45:03 +1100

    Implemented the UI design

--- lib/screens/dockets_screen.dart
+++ lib/screens/dockets_screen.dart
@@ -3,6 +3,101 @@ import 'package:flutter/material.dart';
 class DocketsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(
+                top: 60.0,
+                left: 30.0,
+                right: 30.0,
+                bottom: 30.0,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                    child: Icon(
+                      Icons.list,
+                      size: 35.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    'My Dockets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15.0,
+                  ),
+                  Text(
+                    '12 Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset.zero,
+                      blurRadius: 6.0,
+                      spreadRadius: 3.0,
+                    )
+                  ],
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25.0),
+                    topRight: Radius.circular(25.0),
+                  ),
+                ),
+                child: ListView(
+                  children: <Widget>[
+                    ListTile(
+                      title: Text('My First Docket'),
+                      trailing: Checkbox(
+                        value: false,
+                        onChanged: (bool value) {},
+                      ),
+                    ),
+                    ListTile(
+                      title: Text('My Second Docket'),
+                      trailing: Checkbox(
+                        value: false,
+                        onChanged: (bool value) {},
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
