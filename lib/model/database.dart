bcae1559441494a71b2eb5d1111830f6c6d17181
Prashant <prashant.patel77@gmail.com>
2020-04-17 16:42:59 +1000

    Added DbDockets data class and database for it with moor package.

--- lib/model/database.dart
+++ lib/model/database.dart
@@ -1,53 +1,29 @@
-import 'dart:io';
+import 'package:moor_flutter/moor_flutter.dart';
 
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:sqflite/sqflite.dart';
+part 'database.g.dart';
 
-import 'docket.dart';
+class DbDockets extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get name => text().withLength(min: 1, max: 50)();
+  DateTimeColumn get dueDate => dateTime().nullable()();
+  BoolColumn get completed => boolean().withDefault(Constant(false))();
+}
 
-class DBProvider {
-  DBProvider._();
-  static final DBProvider db = DBProvider._();
-  static Database _database;
+@UseMoor(tables: [DbDockets])
+class AppDatabase extends _$AppDatabase {
+  AppDatabase()
+      : super(FlutterQueryExecutor.inDatabaseFolder(
+          path: 'docketsdb.sqlite',
+          logStatements: true,
+        ));
+  @override
+  int get schemaVersion => 1;
 
-  Future<Database> get database async {
-    if (_database != null) return _database;
-
-    _database = await initDB();
-    return _database;
-  }
-
-  initDB() async {
-    Directory docDir = await getApplicationDocumentsDirectory();
-    String path = join(docDir.path, "DocketsDB.db");
-    return await openDatabase(path, version: 1, onOpen: (db) {},
-        onCreate: (Database db, int version) async {
-      await db.execute("CREATE TABLE Dockets("
-          "id INTEGER PRIMARY KEY,"
-          "name TEXT,"
-          "isDone BIT"
-          ")");
-    });
-  }
-
-  Future<List<Docket>> getAllDockets() async {
-    final db = await database;
-    var res = await db.query("Dockets");
-    List<Docket> list =
-        res.isNotEmpty ? res.map((f) => Docket.fromJson(f)).toList() : [];
-
-    return list;
-  }
-
-  newDocket(Docket docket) async {
-    final db = await database;
-    var table = await db.rawQuery("SELECT MAX(id)+1 as id FROM Dockets");
-    int id = table.first["id"];
-    var raw = await db.rawInsert(
-        "INSERT Into Client(id, name, isDone)"
-        " VALUES(?, ?, ?)",
-        [id, docket.name, docket.isDone]);
-    return raw;
-  }
+  Future<List<DbDocket>> getAllDbDockets() => select(dbDockets).get();
+  Stream<List<DbDocket>> watchAllDbDockets() => select(dbDockets).watch();
+  Future insertDbDocket(DbDocket dbDocket) => into(dbDockets).insert(dbDocket);
+  Future updateDbDocket(DbDocket dbDocket) =>
+      update(dbDockets).replace(dbDocket);
+  Future deleteDbDocket(DbDocket dbDocket) =>
+      delete(dbDockets).delete(dbDocket);
 }
---
+++ lib/model/database.g.dart
@@ -0,0 +1,249 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'database.dart';
+
+// **************************************************************************
+// MoorGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps, unnecessary_this
+class DbDocket extends DataClass implements Insertable<DbDocket> {
+  final int id;
+  final String name;
+  final DateTime dueDate;
+  final bool completed;
+  DbDocket(
+      {@required this.id,
+      @required this.name,
+      this.dueDate,
+      @required this.completed});
+  factory DbDocket.fromData(Map<String, dynamic> data, GeneratedDatabase db,
+      {String prefix}) {
+    final effectivePrefix = prefix ?? '';
+    final intType = db.typeSystem.forDartType<int>();
+    final stringType = db.typeSystem.forDartType<String>();
+    final dateTimeType = db.typeSystem.forDartType<DateTime>();
+    final boolType = db.typeSystem.forDartType<bool>();
+    return DbDocket(
+      id: intType.mapFromDatabaseResponse(data['${effectivePrefix}id']),
+      name: stringType.mapFromDatabaseResponse(data['${effectivePrefix}name']),
+      dueDate: dateTimeType
+          .mapFromDatabaseResponse(data['${effectivePrefix}due_date']),
+      completed:
+          boolType.mapFromDatabaseResponse(data['${effectivePrefix}completed']),
+    );
+  }
+  factory DbDocket.fromJson(Map<String, dynamic> json,
+      {ValueSerializer serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return DbDocket(
+      id: serializer.fromJson<int>(json['id']),
+      name: serializer.fromJson<String>(json['name']),
+      dueDate: serializer.fromJson<DateTime>(json['dueDate']),
+      completed: serializer.fromJson<bool>(json['completed']),
+    );
+  }
+  @override
+  Map<String, dynamic> toJson({ValueSerializer serializer}) {
+    serializer ??= moorRuntimeOptions.defaultSerializer;
+    return <String, dynamic>{
+      'id': serializer.toJson<int>(id),
+      'name': serializer.toJson<String>(name),
+      'dueDate': serializer.toJson<DateTime>(dueDate),
+      'completed': serializer.toJson<bool>(completed),
+    };
+  }
+
+  @override
+  DbDocketsCompanion createCompanion(bool nullToAbsent) {
+    return DbDocketsCompanion(
+      id: id == null && nullToAbsent ? const Value.absent() : Value(id),
+      name: name == null && nullToAbsent ? const Value.absent() : Value(name),
+      dueDate: dueDate == null && nullToAbsent
+          ? const Value.absent()
+          : Value(dueDate),
+      completed: completed == null && nullToAbsent
+          ? const Value.absent()
+          : Value(completed),
+    );
+  }
+
+  DbDocket copyWith({int id, String name, DateTime dueDate, bool completed}) =>
+      DbDocket(
+        id: id ?? this.id,
+        name: name ?? this.name,
+        dueDate: dueDate ?? this.dueDate,
+        completed: completed ?? this.completed,
+      );
+  @override
+  String toString() {
+    return (StringBuffer('DbDocket(')
+          ..write('id: $id, ')
+          ..write('name: $name, ')
+          ..write('dueDate: $dueDate, ')
+          ..write('completed: $completed')
+          ..write(')'))
+        .toString();
+  }
+
+  @override
+  int get hashCode => $mrjf($mrjc(id.hashCode,
+      $mrjc(name.hashCode, $mrjc(dueDate.hashCode, completed.hashCode))));
+  @override
+  bool operator ==(dynamic other) =>
+      identical(this, other) ||
+      (other is DbDocket &&
+          other.id == this.id &&
+          other.name == this.name &&
+          other.dueDate == this.dueDate &&
+          other.completed == this.completed);
+}
+
+class DbDocketsCompanion extends UpdateCompanion<DbDocket> {
+  final Value<int> id;
+  final Value<String> name;
+  final Value<DateTime> dueDate;
+  final Value<bool> completed;
+  const DbDocketsCompanion({
+    this.id = const Value.absent(),
+    this.name = const Value.absent(),
+    this.dueDate = const Value.absent(),
+    this.completed = const Value.absent(),
+  });
+  DbDocketsCompanion.insert({
+    this.id = const Value.absent(),
+    @required String name,
+    this.dueDate = const Value.absent(),
+    this.completed = const Value.absent(),
+  }) : name = Value(name);
+  DbDocketsCompanion copyWith(
+      {Value<int> id,
+      Value<String> name,
+      Value<DateTime> dueDate,
+      Value<bool> completed}) {
+    return DbDocketsCompanion(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      dueDate: dueDate ?? this.dueDate,
+      completed: completed ?? this.completed,
+    );
+  }
+}
+
+class $DbDocketsTable extends DbDockets
+    with TableInfo<$DbDocketsTable, DbDocket> {
+  final GeneratedDatabase _db;
+  final String _alias;
+  $DbDocketsTable(this._db, [this._alias]);
+  final VerificationMeta _idMeta = const VerificationMeta('id');
+  GeneratedIntColumn _id;
+  @override
+  GeneratedIntColumn get id => _id ??= _constructId();
+  GeneratedIntColumn _constructId() {
+    return GeneratedIntColumn('id', $tableName, false,
+        hasAutoIncrement: true, declaredAsPrimaryKey: true);
+  }
+
+  final VerificationMeta _nameMeta = const VerificationMeta('name');
+  GeneratedTextColumn _name;
+  @override
+  GeneratedTextColumn get name => _name ??= _constructName();
+  GeneratedTextColumn _constructName() {
+    return GeneratedTextColumn('name', $tableName, false,
+        minTextLength: 1, maxTextLength: 50);
+  }
+
+  final VerificationMeta _dueDateMeta = const VerificationMeta('dueDate');
+  GeneratedDateTimeColumn _dueDate;
+  @override
+  GeneratedDateTimeColumn get dueDate => _dueDate ??= _constructDueDate();
+  GeneratedDateTimeColumn _constructDueDate() {
+    return GeneratedDateTimeColumn(
+      'due_date',
+      $tableName,
+      true,
+    );
+  }
+
+  final VerificationMeta _completedMeta = const VerificationMeta('completed');
+  GeneratedBoolColumn _completed;
+  @override
+  GeneratedBoolColumn get completed => _completed ??= _constructCompleted();
+  GeneratedBoolColumn _constructCompleted() {
+    return GeneratedBoolColumn('completed', $tableName, false,
+        defaultValue: Constant(false));
+  }
+
+  @override
+  List<GeneratedColumn> get $columns => [id, name, dueDate, completed];
+  @override
+  $DbDocketsTable get asDslTable => this;
+  @override
+  String get $tableName => _alias ?? 'db_dockets';
+  @override
+  final String actualTableName = 'db_dockets';
+  @override
+  VerificationContext validateIntegrity(DbDocketsCompanion d,
+      {bool isInserting = false}) {
+    final context = VerificationContext();
+    if (d.id.present) {
+      context.handle(_idMeta, id.isAcceptableValue(d.id.value, _idMeta));
+    }
+    if (d.name.present) {
+      context.handle(
+          _nameMeta, name.isAcceptableValue(d.name.value, _nameMeta));
+    } else if (isInserting) {
+      context.missing(_nameMeta);
+    }
+    if (d.dueDate.present) {
+      context.handle(_dueDateMeta,
+          dueDate.isAcceptableValue(d.dueDate.value, _dueDateMeta));
+    }
+    if (d.completed.present) {
+      context.handle(_completedMeta,
+          completed.isAcceptableValue(d.completed.value, _completedMeta));
+    }
+    return context;
+  }
+
+  @override
+  Set<GeneratedColumn> get $primaryKey => {id};
+  @override
+  DbDocket map(Map<String, dynamic> data, {String tablePrefix}) {
+    final effectivePrefix = tablePrefix != null ? '$tablePrefix.' : null;
+    return DbDocket.fromData(data, _db, prefix: effectivePrefix);
+  }
+
+  @override
+  Map<String, Variable> entityToSql(DbDocketsCompanion d) {
+    final map = <String, Variable>{};
+    if (d.id.present) {
+      map['id'] = Variable<int, IntType>(d.id.value);
+    }
+    if (d.name.present) {
+      map['name'] = Variable<String, StringType>(d.name.value);
+    }
+    if (d.dueDate.present) {
+      map['due_date'] = Variable<DateTime, DateTimeType>(d.dueDate.value);
+    }
+    if (d.completed.present) {
+      map['completed'] = Variable<bool, BoolType>(d.completed.value);
+    }
+    return map;
+  }
+
+  @override
+  $DbDocketsTable createAlias(String alias) {
+    return $DbDocketsTable(_db, alias);
+  }
+}
+
+abstract class _$AppDatabase extends GeneratedDatabase {
+  _$AppDatabase(QueryExecutor e) : super(SqlTypeSystem.defaultInstance, e);
+  $DbDocketsTable _dbDockets;
+  $DbDocketsTable get dbDockets => _dbDockets ??= $DbDocketsTable(this);
+  @override
+  Iterable<TableInfo> get allTables => allSchemaEntities.whereType<TableInfo>();
+  @override
+  List<DatabaseSchemaEntity> get allSchemaEntities => [dbDockets];
+}
